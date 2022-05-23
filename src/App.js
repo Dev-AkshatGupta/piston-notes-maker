5703338c93a259bb34a81603c96f5ea5496345f8
akshatgupta105 <53713926+Dev-AkshatGupta@users.noreply.github.com>
2022-05-23 10:14:22 +0530

    redux refactored

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
+import AuthenticationPage from "Pages/AuthenticationPage/AuthenticationPage";
 
 
 function App() {
   return (
     <div className="App">
-     <p className="underline">akshat</p>
+  <AuthenticationPage/>
+  
     </div>
   );
 }
